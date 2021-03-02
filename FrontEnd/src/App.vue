afc28113fc71c22718b8ece2583a2d1cc87e7b27
JuneHyung <cjh951114@gmail.com>
2021-03-02 15:55:22 +0900

    App.vue
    헤더, 내용, 푸터로 구성

---
+++ FrontEnd/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <nav-header></nav-header>
+    <router-view></router-view>
+    <nav-footer></nav-footer>
+  </v-app>
+</template>
+
+<script>
+import NavHeader from './components/commons/Header.vue';
+import NavFooter from './components/commons/Footer.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    NavHeader,
+    NavFooter,
+  },
+};
+</script>
+<style>
+* {
+  font-family: 'OpenSans';
+}
+</style>
