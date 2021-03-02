8a4c305e3750e79f7f31052e8496721881f447db
JuneHyung <cjh951114@gmail.com>
2021-03-02 16:21:03 +0900

    ApplicationTest.java
    ApplicationTest.java

---
+++ BackEnd/src/test/java/com/ssafy/bombom/BomBomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.bombom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BomBomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
