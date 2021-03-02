d20b97c583bfd2487ec6aa626cbae0756582137b
JuneHyung <cjh951114@gmail.com>
2021-03-02 15:11:35 +0900

    메인페이지
    메인페이지

---
+++ src/components/views/main.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <v-carousel :show-arrows="false" cycle height="650">
+      <v-carousel-item
+        v-for="(item, i) in items"
+        :key="i"
+        :src="require(`@/assets/images/banners/${item.name}.jpg`)"
+        reverse-transition="fade-transition"
+        transition="fade-transition"
+        class="bannerItem"
+      ></v-carousel-item>
+    </v-carousel>
+    <introduce></introduce>
+  </div>
+</template>
+<script>
+import Introduce from '../main/Introduce.vue';
+export default {
+  data() {
+    return {
+      items: [
+        {
+          name: 'banner01',
+        },
+        {
+          name: 'banner02',
+        },
+        {
+          name: 'banner03',
+        },
+        {
+          name: 'banner04',
+        },
+      ],
+    };
+  },
+  components: {
+    Introduce,
+  },
+};
+</script>
+<style></style>
