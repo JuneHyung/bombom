a4ecec6a56d6f6e909c21c80e579cb3697995c3b
JuneHyung <cjh951114@gmail.com>
2021-03-02 15:59:15 +0900

    쥬스 메뉴 목록
    쥬스 메뉴 목록

---
+++ FrontEnd/src/components/menu/JuiceList.vue
@@ -0,0 +1,37 @@
+<!--쥬스 메뉴 목록-->
+<template>
+  <v-row>
+    <!--반복문으로 클릭했을 때의 메뉴를 보여줌.-->
+    <v-col v-for="item in items" :key="item.menuNo" cols="12" md="3">
+      <v-card>
+        <v-img :src="require(`@/assets/images/menu/juice/${item.menuImg}`)" aspect-ratio="1" />
+        <v-card-title class="title">
+          <v-spacer></v-spacer>{{ item.menuName }} {{ item.menuPrice }} <v-spacer></v-spacer>
+        </v-card-title>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+<script>
+import http from '@/util/http-common';
+
+export default {
+  data() {
+    return {
+      items: [],
+    };
+  },
+  // 쥬스 목록을 items에 담는다
+  created() {
+    http
+      .get('/menu/juice')
+      .then(({ data }) => {
+        this.items = data;
+      })
+      .catch(() => {
+        alert('에러가 발생하였습니다.');
+      });
+  },
+};
+</script>
+<style></style>
