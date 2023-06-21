c234dfa11d5c12b5b07e03a0a803111b6896e6d6
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2023-06-21 12:05:23 +0900

    feat : 공지사항 data init

--- FrontEnd/src/pages/notice/NoticePage.vue
+++ FrontEnd/src/pages/notice/NoticePage.vue
@@ -1,3 +1,31 @@
 <template>
-  <p>공지사항 페이지 입니다.</p>
+  <div>
+    <p>공지사항 페이지 입니다.</p>
+    <button>등록</button>
+  </div>
+  <table>
+    <thead>
+      <th>글 번호</th>
+      <th>제목</th>
+      <th>작성일</th>
+      <th>조회수</th>
+      <th>Actions</th>
+    </thead>
+    <tbody>
+      <template v-for="(item) in noticeList" :key="item">
+        <tr>
+          <td>{{ item.noticeNo }}</td>
+          <td>{{ item.noticeTitle }}</td>
+          <td>{{ item.noticeDate }}</td>
+          <td>{{ item.noticeView }}</td>
+          <td>수정, 삭제</td>
+        </tr>
+      </template>
+    </tbody>
+  </table>
 </template>
+
+<script setup>
+import noticeData from '@/constant/noticeData.json'
+const noticeList = noticeData;
+</script>
