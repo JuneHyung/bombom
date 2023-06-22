4d4a2f00f3c08506b331fdbd9518772129700ae3
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2023-06-22 20:22:16 +0900

    feat : menu list &item design (hover효과관련 색상 조정 필요)

--- FrontEnd/src/pages/menu/MenuPage.vue
+++ FrontEnd/src/pages/menu/MenuPage.vue
@@ -1,15 +1,27 @@
 <template>
-  <p>메뉴 정보 페이지 입니다.</p>
-  <div>
-    <ul>
+  <div class="menu-wrap">
+    <ul class="menu-tab">
       <template v-for="(menuType, idx) in menuTypeList" :key="idx">
-        <li>{{ menuType }}</li>
+        <li
+          class="menu-tab-item"
+          :class="{ 'active-item': curIdx === idx }"
+          @click="changeCurIdx(idx)"
+        >
+          {{ menuType }}
+        </li>
       </template>
     </ul>
-    <ul>
+    <ul class="menu-tab-pane">
       <template v-for="(item, idx) in menuList" :key="idx">
-        <p>{{ item.menuName }}</p>
-        <p>{{ item.menuPrice }}</p>
+        <li class="menu-tab-pane-item">
+          <div class="menu-image-box">
+            <img :src="item.menuImg" :alt="item.menuName" />
+          </div>
+          <p class="menu-info-text">
+            <span class="menu-item-name">{{ item.menuName }}</span>
+            <span class="menu-item-price">{{ item.menuPrice }}</span>
+          </p>
+        </li>
       </template>
     </ul>
   </div>
@@ -17,8 +29,9 @@
 
 <script setup>
 import menuData from '@/constant/menuData.json'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
+const curIdx = ref(0)
 const menuTypeList = menuData.map((el) => el.type)
-const menuList = ref(menuData.map((el) => el.list)[0])
-
+const menuList = computed(() => menuData.map((el) => el.list)[curIdx.value])
+const changeCurIdx = (idx) => (curIdx.value = idx)
 </script>
