2f5c22061e44ef4c901616de1e1748d834ad10b4
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2023-06-29 20:26:29 +0900

    feat : 로그아웃버튼 디자인

--- FrontEnd/src/components/common/header/AfterLogInItem.vue
+++ FrontEnd/src/components/common/header/AfterLogInItem.vue
@@ -4,7 +4,7 @@
       {{userStore.userInfo.userName}}님 ({{ userStore.userInfo.userId }}) 님 환영합니다.
     </RouterLink>
   </li>
-  <button @click="logout">LOGOUT</button>
+  <button class="logout-button" @click="logout">LOGOUT</button>
 </template>
 <script setup>
 import {useUserStore} from '@/stores/user.js';
