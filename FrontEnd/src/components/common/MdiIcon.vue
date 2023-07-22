894032adbf6df267cd115ed0d2ec681e4bb46a78
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2023-07-22 19:57:34 +0900

    fix: MdiIcon 라이브러리 변경 및 적용.

--- FrontEnd/src/components/common/MdiIcon.vue
+++ FrontEnd/src/components/common/MdiIcon.vue
@@ -1,18 +1,14 @@
 <template>
-  <SvgIcon type="mdi" :path="path" class="cursor-pointer"></SvgIcon>
+  <svg width="24" height="24" viewBox="0 0 24 24" class="cursor-pointer">
+    <path :d="mdiIcons[props.name as keyof typeof mdiIcons]"></path>
+  </svg>
 </template>
-<script setup>
-import SvgIcon from '@jamescoyle/vue-icon'
-import { mdiEyeOutline, mdiPencil, mdiTrashCanOutline, mdiAlertDecagramOutline } from '@mdi/js'
-import { computed} from 'vue';
-const props = defineProps({
-  name: String,
-})
+<script setup lang="ts">
+import * as mdiIcons from "@mdi/js";
+interface Props{
+  name: string,
+}
 
-const path = computed(()=>{
-  if(props.name ==='mdi-eye-outline') return mdiEyeOutline;
-  else if(props.name ==='mdi-pencil') return mdiPencil;
-  else if(props.name ==='mdi-trash-can-outline') return mdiTrashCanOutline;
-  else return mdiAlertDecagramOutline;
-});
+const props = defineProps<Props>()
+
 </script>
