f7fe6c3ebf74dc4fdebdc0b7ca8d4829bd26a476
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2023-07-22 20:26:22 +0900

    fix : kakaoMap init 위치 App.vue로 변경

--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -10,4 +10,17 @@
 <script setup>
 import NavHeader from '@/components/common/header/NavHeader.vue';
 import NavFooter from '@/components/common/footer/NavFooter.vue';
+import { onMounted } from 'vue';
+const initScript = () => {
+  if (!window.kakao || !window.kakao.maps) {
+    const script = document.createElement('script')
+    script.type = 'text/javascript'
+    const key = import.meta.env.VITE_KAKAO_MAP_API_KEY
+    script.src = `//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${key}&libraries=services`
+    document.head.appendChild(script)
+  }
+}
+onMounted(()=>{
+  initScript();
+})
 </script>
